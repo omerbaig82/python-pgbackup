ce4e96b1f272a38a3edf7b05e9b59acc783cffba
Omer Baig <Omer.Baig@willistowerswatson.com>
2022-06-04 18:38:19 +0000

    Altered path of pg_dump

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -3,7 +3,7 @@ import sys
 
 def dump(url):
     try:
-        return subprocess.Popen(['/usr/pgsql-10/bin/pg_dump', url], stdout=subprocess.PIPE)
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
     except OSError as err:
         print(f"Error: {err}")
         sys.exit(1)
